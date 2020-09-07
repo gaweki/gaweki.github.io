6819e083e8aaaddca154de46bff1285518c13c33
andreltokdis <andrel.karunia.s@mail.ugm.ac.id>
2020-09-07 21:25:32 +0700

    chore: Update tailwind inset

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ const { colors, width } = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   theme: {
+    inset: {
+      '0': 0,
+      '24': '6.9rem',
+      '7': '1.9rem'
+    },
     extend: {
       colors: {
         blue: {
