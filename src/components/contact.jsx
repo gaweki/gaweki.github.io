81e3bb994fe088331db629d78ec907f854fd3ab8
andreltokdis <andrel.karunia.s@mail.ugm.ac.id>
2020-09-17 21:22:50 +0700

    style: Add shadow button contacts

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -20,9 +20,9 @@ const Contact = () => {
         </div>
 
         <div className={`absolute flex justify-end bottom-20 w-full ${show ? `block` : `hidden`}`}>
-          <div className="right-8 relative">
-            <a className={`pb-3`} href="https://twitter.com/messages/compose?recipient_id=963052414264688640&text=Hello%20Andrel">
-              <svg width="40" height="40" fill="rgba(29,161,242,1.00)" viewBox="0 0 25 25">
+          <div className="right-10 relative">
+            <a className={`pb-3 block`} href="https://twitter.com/messages/compose?recipient_id=963052414264688640&text=Hello%20Andrel">
+              <svg className="filter-shadow-md" width="40" height="40" fill="rgba(29,161,242,1.00)" viewBox="0 0 25 25">
                 <g>
                   <path
                     d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"
@@ -30,8 +30,8 @@ const Contact = () => {
                 </g>
               </svg>
             </a>
-            <a className={`pb-3`} href="https://m.facebook.com/messages/compose?ids=100011595496988">
-              <svg viewBox="0 0 36 36" fill="url(#jsc_s_2)" height="40" width="40">
+            <a className={`pb-3 block`} href="https://m.facebook.com/messages/compose?ids=100011595496988">
+              <svg className="filter-shadow-md" viewBox="0 0 36 36" fill="url(#jsc_s_2)" height="40" width="40">
                 <defs>
                   <linearGradient x1="50%" x2="50%" y1="97.0782153%" y2="0%" id="jsc_s_2">
                     <stop offset="0%" stopColor="#0062E0"></stop>
@@ -43,7 +43,7 @@ const Contact = () => {
               </svg>
             </a>
             <a href="https://mail.google.com/mail/?view=cm&fs=1&to=andrel.sitanggang@gmail.com&su=Hello%20Andrel">
-              <img className="w-10" src="https://img.icons8.com/color/2x/gmail.png" alt="Email andrel" />
+              <img className="shadow-md w-10" src="https://img.icons8.com/color/2x/gmail.png" alt="Email andrel" />
             </a>
           </div>
           </div>
